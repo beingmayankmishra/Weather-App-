effe6a3bbee45c4d90b6da080dfe2ccb7d37bd84
beingmayankmishra <beingmayankmishra@gmail.com>
2024-09-02 23:01:01 +0530

    changes in js

---
+++ script.js
@@ -0,0 +1,7 @@
+const userTab=document.querySelector("[data-userWeather]");
+const searchtab=document.querySelector("[data-searchWeather]");
+const userContainer=documnet.querySelector(".weather-container");
+const grantAccessContainer=document.querySelector(".grant-location-container");
+const searchForm=document.querySelector("[data-searchForm]");
+const loadingScreen=document.querySelector(".loading-container");
+const userInfoContainer=document.querySelector(".user-info-container");
