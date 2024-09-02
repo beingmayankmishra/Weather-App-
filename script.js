7ea3923934cb0d963362c8fff8f32e3281feedbe
beingmayankmishra <beingmayankmishra@gmail.com>
2024-09-03 00:34:38 +0530

    4th commit

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-const userTab=document.querySelector("[data-userWeather]");
-const searchtab=document.querySelector("[data-searchWeather]");
-const userContainer=documnet.querySelector(".weather-container");
-const grantAccessContainer=document.querySelector(".grant-location-container");
-const searchForm=document.querySelector("[data-searchForm]");
-const loadingScreen=document.querySelector(".loading-container");
-const userInfoContainer=document.querySelector(".user-info-container");
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+
+let currentTab=userTab;
+const API_KEY="bf1c1e11da63a26595558bc958171864";
+currentTab.classList.add("current-tab");
+
+function switchTab(clickedTab){
+    if(clickedTab!=currentTab){
+        currentTab.classList.remove("current-tab");
+        currentTab=clickedTab;
+        currentTab.classList.add("current-tab");
+    }
+
+}
+
+userTab.addEventListener("click", () => {
+    
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+    switchTab(searchTab);
+});
+
